a4920f89fce851e4e7e9002ed2efe427fb81f7c5
Renée Kooi <renee@kooi.me>
2018-05-24 11:32:51 +0200

    Deploy to Github Pages

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,10 @@
 const TEST = process.env.BABEL_ENV === 'test';
+const CJS = process.env.BABEL_ENV === 'cjs';
 
 module.exports = {
   presets: [
     ['@babel/env', {
-      modules: TEST ? 'commonjs' : false,
+      modules: TEST || CJS ? 'commonjs' : false,
       loose: true,
       targets: TEST ? { node: 'current' } : {},
     }],
