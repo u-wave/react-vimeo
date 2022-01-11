326decf716ef7f2652788e64170fbf1956681b97
Nguyễn Thanh Sơn <son@gotitapp.co>
2022-01-11 11:56:42 +0700

    feat: Add new constructor options from props

--- src/index.js
+++ src/index.js
@@ -47,6 +47,12 @@ class Vimeo extends React.Component {
       responsive: this.props.responsive,
       dnt: this.props.dnt,
       speed: this.props.speed,
+      keyboard: this.props.keyboard,
+      pip: this.props.pip,
+      playsinline: this.props.playsinline,
+      quality: this.props.quality,
+      texttrack: this.props.texttrack,
+      transparent: this.props.transparent,
     };
     /* eslint-enable react/destructuring-assignment */
   }
@@ -290,6 +296,40 @@ if (process.env.NODE_ENV !== 'production') {
      * Enable playback rate controls (requires Vimeo PRO / Business account)
      */
     speed: PropTypes.bool,
+
+    /**
+     * Allows for keyboard input to trigger player events.
+     */
+    keyboard: PropTypes.bool,
+
+    /**
+     * Show the picture-in-picture button in the controlbar
+     * and enable the picture-in-picture API.
+     */
+    pip: PropTypes.bool,
+
+    /**
+     * Play video inline on mobile devices, to automatically
+     * go fullscreen on playback set this parameter to false.
+     */
+    playsinline: PropTypes.bool,
+
+    /**
+     * Vimeo Plus, PRO, and Business members can default
+     * an embedded video to a specific quality on desktop.
+     */
+    quality: PropTypes.string,
+
+    /**
+     * Turn captions/subtitles on for a specific language by default.
+     */
+    texttrack: PropTypes.string,
+
+    /**
+     * The responsive player and transparent background are enabled
+     * by default, to disable set this parameter to false.
+     */
+    transparent: PropTypes.bool,
 
     // Events
     /* eslint-disable react/no-unused-prop-types */
